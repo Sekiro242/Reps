2f13edff04165c482e227ccf0f22e5c3bd57b34d
Saif3 <Seif.523018@gmail.com>
2024-11-14 07:53:59 +0200

    MyProject

---
+++ AllMovies.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for AllMovies.xaml
+    /// </summary>
+    public partial class AllMovies : Page
+    {
+       MoviesystemEntities db = new MoviesystemEntities();
+        public AllMovies()
+        {
+            InitializeComponent();
+            DG1.ItemsSource = db.Movies.Select(x => new {x.MovieID,x.MovieName,x.MovieCategory}).ToList();
+        }
+
+        private void Button_Click(object sender, object e)
+        {
+            DG1.ItemsSource = db.Movies.Where(x=> x.MovieCategory == comboboxcate.Text).Select(x=> new {x.MovieID,x.MovieName, x.MovieCategory}).ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MovieDetails details = new MovieDetails();
+            Movies movie = db.Movies.FirstOrDefault(x => x.MovieID == int.Parse(MovieIDTXT.Text));
+            details.NameLabel.Content = movie.MovieName;
+            details.CategoryLabel.Content = movie.MovieCategory;
+
+            
+          
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        MoviesystemEntities db = new MoviesystemEntities();
+        public Login()
+        {
+
+
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            this.NavigationService.Navigate(signUp);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ForgetPass forgetPass    = new ForgetPass();
+            this.NavigationService.Navigate(forgetPass);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Users users = new Users();
+            string Emaile = Emailb.Text;
+            string Passwordd = Passb.Text;
+            users = db.Users.FirstOrDefault(x => x.Email == Emaile &&x.UserPassword == Passwordd);
+            string AdmingE = "Saif.523018@gmail.com";
+            string AdminP = "12341234";
+            
+            
+            if(users != null)
+            {
+                AllMovies allMovies = new AllMovies();
+                this.NavigationService.Navigate(allMovies);
+
+            }
+            else if(Emaile == AdmingE && Passwordd == AdminP) {
+                AdminPage adminPage = new AdminPage();
+                this.NavigationService.Navigate(adminPage);
+            }
+            else
+            {
+                MessageBox.Show("Wrong Email");
+            }
+            
+          
+        }
+
+        
+    }
+}
